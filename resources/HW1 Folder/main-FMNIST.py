5e47a58f1af981731d100aaca401487b0c49c589
R3trib4tion <46800799+R3trib4tion@users.noreply.github.com>
2021-02-25 05:51:55 -0600

    Add files via upload

--- resources/HW1 Folder/main-FMNIST.py
+++ resources/HW1 Folder/main-FMNIST.py
@@ -1,14 +1,21 @@
 import tensorflow as tf
 from tensorflow import keras
 import pandas as pd
+import math as m
 import matplotlib.pyplot as plt    # to visualize data and draw plots
+import numpy as np
 
 fashion_mnist = keras.datasets.fashion_mnist
 
 (images, labels), (temp, temp1) = fashion_mnist.load_data()
-temp = temp /255.0
-train_images, validation_images = images[5000:] / 255.0, images[:5000] / 255.0
-train_labels, validation_labels = labels[5000:], labels[:5000]
+
+# Major changes here: I partitioned 50,000 images to be train and 10,000 images to be validation.
+# This caused the results to be significantly different for some models. 
+# I will make changes to the report as needed.
+train_images, validation_images= images[50000:] / 255.0, images[:10000] / 255.0  
+train_labels, validation_labels= labels[50000:], labels[:10000]
+
+classes=[0,1,2,3,4,5,6,7,8,9] # To make heat map
 
 invertedTriangle = tf.keras.models.Sequential([
     keras.layers.Flatten(input_shape=(28, 28, 1)),
@@ -51,22 +58,42 @@ hexagon = tf.keras.models.Sequential([
 
 model = invertedTriangle # Change based on the model you want to test
 model.compile(loss = "sparse_categorical_crossentropy", 
-              optimizer = "adam",
-              metrics = ["accuracy"])
+              optimizer = "adam", metrics = ["accuracy"])
 
 history = model.fit(train_images, train_labels, batch_size = 32, epochs=100, 
           validation_data=(validation_images, validation_labels), use_multiprocessing=(True))
 
-model.evaluate(temp, temp1)
+# Evaluates model on validation data and graph it
+evaluation = model.evaluate(validation_images, validation_labels) # Stores [loss, accuracy] of model
 pd.DataFrame(history.history).plot(figsize=(8,5))
+plt.xlabel("Epoch")
+plt.ylabel("Percentage")
 plt.grid(True)
 plt.gca().set_ylim(0,1)
 plt.show()
-    
 
+# Create confidence interval for error rate on validation data
+error_test_data = 0
+y_pred=model.predict_classes(validation_images)   # Make prediction
+for length in range(len(y_pred)):                 # For loop that counts how many correct predictions
+    if y_pred[length] == validation_labels[length]:   
+        error_test_data +=  1/len(validation_labels)
+        
+error_test_data = 1 - error_test_data     # 1 - correct predictions to get rate of wrong predictions
 
+# Make upper and lower bound of error on test data assuming error follows a normal distribution
+upperbound_gen_error = error_test_data + 1.96*(m.sqrt(((error_test_data)*(1 - error_test_data))/len(validation_labels)))  
+lowerbound_gen_error = error_test_data - 1.96*(m.sqrt(((error_test_data)*(1 - error_test_data))/len(validation_labels)))
+print("Based on using normal distribution as the estimator, the 95% confidence interval for error hypothesis: [", lowerbound_gen_error, ",", upperbound_gen_error, "]")
 
+# Making the confusion matrix based on the website: https://androidkt.com/keras-confusion-matrix-in-tensorboard/
+# Feel free to check the website to make any changes to the code. We need to change this since it's exact copy
+con_Matrix = tf.math.confusion_matrix(labels=validation_labels, predictions=y_pred).numpy()
 
+con_Matrix_norm = np.around(con_Matrix.astype('float') / con_Matrix.sum(axis=1)[:, np.newaxis], decimals=2)
 
+con_mat_df = pd.DataFrame(con_Matrix_norm,
+                     index = classes, 
+                     columns = classes)
 
 
--- resources/HW1 Folder/util-FMNIST.py
+++ resources/HW1 Folder/util-FMNIST.py
@@ -14,14 +14,37 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 
 (images, labels), (temp, temp1) = fashion_mnist.load_data()
-temp = temp /255.0
-train_images, validation_images = images[5000:] / 255.0, images[:5000] / 255.0
-train_labels, validation_labels = labels[5000:], labels[:5000]
+train_images, validation_images= images[50000:] / 255.0, images[:10000] / 255.0  
+train_labels, validation_labels= labels[50000:], labels[:10000]
 
-
-model.evaluate(temp, temp1)
+evaluation = model.evaluate(validation_images, validation_labels)
 pd.DataFrame(history.history).plot(figsize=(8,5))
+plt.xlabel("Epoch")
+plt.ylabel("Percentage")
 plt.grid(True)
 plt.gca().set_ylim(0,1)
 plt.show()
 
+# Create confidence interval for error rate on validation data
+error_test_data = 0
+y_pred=model.predict_classes(validation_images)   # Make prediction
+for length in range(len(y_pred)):                 # For loop that counts how many correct predictions
+    if y_pred[length] == validation_labels[length]:   
+        error_test_data +=  1/len(validation_labels)
+        
+error_test_data = 1 - error_test_data     # 1 - correct predictions to get rate of wrong predictions
+
+# Make upper and lower bound of error on test data assuming error follows a normal distribution
+upperbound_gen_error = error_test_data + 1.96*(m.sqrt(((error_test_data)*(1 - error_test_data))/len(validation_labels)))  
+lowerbound_gen_error = error_test_data - 1.96*(m.sqrt(((error_test_data)*(1 - error_test_data))/len(validation_labels)))
+print("Based on using normal distribution as the estimator, the 95% confidence interval for error hypothesis: [", lowerbound_gen_error, ",", upperbound_gen_error, "]")
+
+# Making the confusion matrix based on the website: https://androidkt.com/keras-confusion-matrix-in-tensorboard/
+# Feel free to check the website to make any changes to the code. We need to change this since it's exact copy
+con_Matrix = tf.math.confusion_matrix(labels=validation_labels, predictions=y_pred).numpy()
+
+con_Matrix_norm = np.around(con_Matrix.astype('float') / con_Matrix.sum(axis=1)[:, np.newaxis], decimals=2)
+
+con_mat_df = pd.DataFrame(con_Matrix_norm,
+                     index = classes, 
+                     columns = classes)
