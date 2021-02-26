a24982a3fce807c59b648a4d51f1b1b9cc9cf54e
R3trib4tion <46800799+R3trib4tion@users.noreply.github.com>
2021-02-25 22:08:47 -0600

    Add files via upload

--- resources/HW1 Folder/model-FMNIST.py
+++ resources/HW1 Folder/model-FMNIST.py
@@ -3,7 +3,7 @@ from tensorflow import keras
 import pandas as pd
 import matplotlib.pyplot as plt    # to visualize data and draw plots
 
-
+# The three model
 invertedTriangle = tf.keras.models.Sequential([
     keras.layers.Flatten(input_shape=(28, 28, 1)),
     
@@ -43,11 +43,14 @@ hexagon = tf.keras.models.Sequential([
     keras.layers.Dense(10, activation="softmax"),
     ])
 
-model = invertedTriangle # Change based on the model 
+
+# In[4]:
+
+
+model = hexagon # Change based on the model you want to test
 model.compile(loss = "sparse_categorical_crossentropy", 
-              optimizer = "adam",
-              metrics = ["accuracy"])
+              optimizer = "adam", metrics = ["accuracy"])
 
 history = model.fit(train_images, train_labels, batch_size = 32, epochs=100, 
           validation_data=(validation_images, validation_labels), use_multiprocessing=(True))
-
+evaluation = model.evaluate(validation_images, validation_labels) # Stores [loss, accuracy] of model
